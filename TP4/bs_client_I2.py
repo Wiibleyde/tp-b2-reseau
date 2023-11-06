12ca04ad548765acafaf13dd833e65905971e9dd
Wiibleyde <nathan@bonnell.fr>
2023-11-06 11:16:17 +0100

    Refactored connect function to use context manager
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- TP4/bs_client_I2.py
+++ TP4/bs_client_I2.py
@@ -2,21 +2,16 @@ import socket
 from time import sleep
 
 def connect(ip, port=13337):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        s.connect((ip, port))
-    except:
-        print("Connection failed")
-        return
-    try:
-        print(f"Connecté avec succès au serveur {ip} sur le port {port}")
-        s.send(input("Que veux-tu envoyer au serveur : ").encode())
-        print(s.recv(1024).decode())
-    except:
-        print("Le serveur a fermé la connexion")
-        s.close()
-        return
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((ip, port))
+            print(f"Connecté avec succès au serveur {ip} sur le port {port}")
+            s.send(input("Que veux-tu envoyer au serveur : ").encode())
+            print(s.recv(1024).decode())
+    except socket.error:
+        print("La connexion a échoué")
+    except Exception as e:
+        print(f"Une erreur s'est produite: {e}")
 
 if __name__ == '__main__':
     connect('10.1.1.10')
-        
