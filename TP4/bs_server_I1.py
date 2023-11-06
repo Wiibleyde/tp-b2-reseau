1362f3d47943c7b132eaffd1e88e5281359497b7
Wiibleyde <nathan@bonnell.fr>
2023-11-06 09:45:07 +0100

    bs-client + server V1
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

---
+++ TP4/bs_server_I1.py
@@ -0,0 +1,13 @@
+import socket
+
+def listen(ip, port=13337):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((ip, port))
+    s.listen(1)
+    conn, addr = s.accept()
+    while True:
+        if conn.recv(1024).decode() == 'Hello':
+            conn.send("Hi mate !".encode())
+
+if __name__ == '__main__':
+    listen('10.1.1.10')
