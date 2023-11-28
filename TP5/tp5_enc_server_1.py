706a9d54e5a2802ad4ee0b5bd1ea8e445ef94435
Wiibleyde <nathan@bonnell.fr>
2023-11-28 12:17:25 +0100

    Add debug tests
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- TP5/tp5_enc_server_1.py
+++ TP5/tp5_enc_server_1.py
@@ -33,6 +33,8 @@ def listen(ip, port=13337, timeout=60):
                 sign = "-"
             elif sign == 2:
                 sign = "*"
+            logger.debug(int.from_bytes(nb1Size, 'big'))
+            logger.debug(int.from_bytes(nb2Size, 'big'))
             calc = conn.recv(int.from_bytes(nb1Size, 'big')+int.from_bytes(nb2Size, 'big'))
             # nb1 = calc[3:3+int.from_bytes(nb1Size, 'big')]
             # nb2 = calc[3+int.from_bytes(nb1Size, 'big'):3+int.from_bytes(nb1Size, 'big')+int.from_bytes(nb2Size, 'big')]
