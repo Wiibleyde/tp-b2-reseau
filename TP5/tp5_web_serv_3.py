7cd718308bd3bc937b24460fc84a18e623959a37
Wiibleyde <nathan@bonnell.fr>
2023-11-30 10:57:59 +0100

    Fix file path in web server code
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- TP5/tp5_web_serv_3.py
+++ TP5/tp5_web_serv_3.py
@@ -31,7 +31,7 @@ def listen(ip, port=13337, timeout=60):
                     conn.send("HTTP/1.0 200 OK\n\n<h1>Hello je suis un serveur HTTP</h1>".encode())
                 else:
                     try:
-                        with open(f"./pages/{splitted[1][1:]}", "r") as f:
+                        with open(f"./pages{splitted[1][1:]}", "r") as f:
                             logger.info(f"Envoi du fichier {splitted[1][1:]} au client {addr}.")
                             conn.send("HTTP/1.0 200 OK\n\n".encode() + f.read().encode())
                     except FileNotFoundError:
