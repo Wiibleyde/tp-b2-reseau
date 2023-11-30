f06cf3cf15ac6b13cbff5b7d55a15312c982a45b
Wiibleyde <nathan@bonnell.fr>
2023-11-30 09:01:26 +0100

    Copy paste files because i already done the integer part
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

---
+++ TP5/tp5_enc_client_2.py
@@ -0,0 +1,80 @@
+import socket
+import sys
+
+from src.logs import Logger
+
+def connect(ip:str, port:int=13337):
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((ip, port))
+            logger.info(f"Connecté réussie à {ip}:{port}")
+
+            message = str(input("Quel calcul souhaitez vous envoyer ? : "))
+
+            if testIfNumberAreValid(message):
+                encoded = message.encode(encoding='utf-8')
+                splitted = message.split(" ")
+                nb1Size, nb2Size, sign = calcNumberSize(int(splitted[0]), int(splitted[2]), splitted[1])
+                header = nb1Size.to_bytes(1, 'big') + nb2Size.to_bytes(1, 'big') + sign.to_bytes(1, 'big')
+                end = 0
+                end = end.to_bytes(1, 'big')
+                s.send(header + int(splitted[0]).to_bytes(nb1Size, 'big') + int(splitted[2]).to_bytes(nb2Size, 'big') + end)
+                logger.info(f"Message envoyé au serveur {ip}:{port} : {header + int(splitted[0]).to_bytes(nb1Size, 'big') + int(splitted[2]).to_bytes(nb2Size, 'big') + end}")
+
+                answer = s.recv(1024).decode()
+                logger.info(f"Réponse du serveur {ip}:{port} : {answer}")
+            else:
+                exit(1)
+    except socket.error:
+        logger.critical(f"Impossible de se connecter à {ip}:{port}")
+        exit(1)
+
+def testIfNumberAreValid(calcul:int) -> bool:
+    nbs = calcul.split(" ")
+    if len(nbs) != 3:
+        logger.error(f"Le calcul doit être de la forme 'nombre1 [+ | - | *] nombre2'.")
+        return False
+    
+    if nbs[1] not in ["+", "-", "*"]:
+        logger.error(f"Le calcul doit être de la forme 'nombre1 [+ | - | *] nombre2'.")
+        return False
+    
+    try:
+        int(nbs[0])
+        int(nbs[2])
+    except ValueError:
+        logger.error(f"Les nombres doivent être des entiers.")
+        return False
+    
+    if int(nbs[0]) > 4294967295 or int(nbs[0]) < -4294967295 or int(nbs[2]) > 4294967295 or int(nbs[2]) < -4294967295:
+        logger.error(f"Les nombres doivent être compris entre -4294967295 et 4294967295.")
+        return False
+    
+    return True
+
+def calcMessageSize(message: bytes) -> int:
+    return len(message)
+
+def calcNumberSize(nb1:int, nb2:int, sign:str) -> tuple:
+    if sign == "+":
+        sign = 0
+    elif sign == "-":
+        sign = 1
+    elif sign == "*":
+        sign = 2
+    nbsList = [nb1, nb2]
+    return (len(nbsList[0].to_bytes(4, 'big')), len(nbsList[1].to_bytes(4, 'big')), sign)
+
+def decodeMessage(message: bytes) -> str:
+    size = message[:2]
+    nb1Size = size[0:1]
+    nb2Size = size[1:2]
+    sign = message[2:3]
+    nb1 = message[3:3+int.from_bytes(nb1Size, 'big')]
+    nb2 = message[3+int.from_bytes(nb1Size, 'big'):3+int.from_bytes(nb1Size, 'big')+int.from_bytes(nb2Size, 'big')]
+    end = message[3+int.from_bytes(nb1Size, 'big')+int.from_bytes(nb2Size, 'big'):]
+    return f"{int.from_bytes(nb1, 'big')} {int.from_bytes(sign, 'big')} {int.from_bytes(nb2, 'big')}"
+
+if __name__ == '__main__':
+    logger = Logger("./logs/bs_client.log", True)
+    connect('10.1.1.10')
---
+++ TP5/tp5_enc_server_2.py
@@ -0,0 +1,87 @@
+import socket
+import sys
+import time
+import argparse
+from src.logs import Logger
+
+def listen(ip, port=13337, timeout=60):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    s.bind((ip, port))
+    s.listen(1)
+    s.setblocking(0) 
+    logger.info(f"Le serveur tourne sur {ip}:{port}")
+    
+    start_time = time.time()
+
+    while True:
+        try:
+            conn, addr = s.accept()
+            logger.info(f"Un client {addr} s'est connecté.")
+
+            header = conn.recv(3)
+            size = header[:2]
+            nb1Size = size[0:1]
+            nb2Size = size[1:2]
+            sign = header[2:3]
+            decodedSign = int.from_bytes(sign, 'big')
+            if decodedSign == 0:
+                sign = "+"
+            elif decodedSign == 1:
+                sign = "-"
+            elif decodedSign == 2:
+                sign = "*"
+            
+            calc = conn.recv(int.from_bytes(nb1Size, 'big')+int.from_bytes(nb2Size, 'big'))
+            nb1 = calc[:int.from_bytes(nb1Size, 'big')]
+            nb2 = calc[int.from_bytes(nb1Size, 'big'):int.from_bytes(nb1Size, 'big')+int.from_bytes(nb2Size, 'big')]
+            calcul = f"{int.from_bytes(nb1, 'big')}{sign}{int.from_bytes(nb2, 'big')}"
+            logger.info(f"Calcul reçu du client {addr} : {calcul}")
+            answer = str(eval(calcul))
+            conn.send(answer.encode())
+            logger.info(f"Réponse envoyée au client {addr} : {answer}")
+            
+            end = conn.recv(1)
+            if end == b'\x00':
+                logger.info(f"Le client {addr} a envoyé un message de fin, fermeture de la connexion.")
+            else:
+                logger.warning(f"Le client {addr} n'a pas envoyé de message de fin / le message de fin n'est pas correct, fermeture de la connexion.")
+            conn.close()
+
+            start_time = time.time()
+        except socket.error as e:
+            if e.errno == 11:
+                pass
+            else:
+                raise
+
+        except KeyboardInterrupt:
+            s.close()
+            logger.info("Le serveur a été arrêté.")
+            exit(0)
+
+        if time.time() - start_time > timeout:
+            logger.warning(f"Aucun client depuis plus de {timeout} secondes.")
+            start_time = time.time()
+
+def parseArgs():
+    parser = argparse.ArgumentParser(description="Serveur de la partie II du TP4")
+    parser.add_argument('-p', '--port', type=int, default=13337, help="Port d'écoute du serveur entre 1024 et 65535 (13337 par défaut)")
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    logger = Logger("/var/log/bs_server/bs_server.log")
+    args = parseArgs()
+
+    if args.port < 0 or args.port > 65535:
+
+        if args.port < 1024:
+            logger.critical("ERROR Le port spécifié est un port privilégié. Spécifiez un port au-dessus de 1024.")
+            exit(2)
+
+        else:
+            logger.critical("ERROR Le port spécifié n'est pas un port possible (de 0 à 65535).")
+            exit(1)
+
+    listen('10.1.1.10', args.port, 60)
