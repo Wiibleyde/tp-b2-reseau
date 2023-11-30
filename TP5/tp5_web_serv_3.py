464264a6e1a351b8f3d13b018968d1b5b6919de3
Wiibleyde <nathan@bonnell.fr>
2023-11-30 11:00:12 +0100

    Add logging statement to retrieve requested file
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- TP5/tp5_web_serv_3.py
+++ TP5/tp5_web_serv_3.py
@@ -31,6 +31,7 @@ def listen(ip, port=13337, timeout=60):
                     conn.send("HTTP/1.0 200 OK\n\n<h1>Hello je suis un serveur HTTP</h1>".encode())
                 else:
                     try:
+                        logger.debug(splitted[1][1:])
                         with open(f"./pages{splitted[1][1:]}", "r") as f:
                             logger.info(f"Envoi du fichier {splitted[1][1:]} au client {addr}.")
                             conn.send("HTTP/1.0 200 OK\n\n".encode() + f.read().encode())
