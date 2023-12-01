b59a2e4a1cb196eb9d1907d138fbcac330b15c77
Wiibleyde <nathan@bonnell.fr>
2023-12-01 10:51:39 +0100

    Refactor client and server code for improved message handling
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- TP5/tp5_enc_server_bonus.py
+++ TP5/tp5_enc_server_bonus.py
@@ -21,22 +21,9 @@ def listen(ip, port=13337, timeout=60):
             logger.info(f"Un client {addr} s'est connecté.")
 
             header = conn.recv(3)
-            size = header[:2]
-            nb1Size = size[0:1]
-            nb2Size = size[1:2]
-            sign = header[2:3]
-            decodedSign = int.from_bytes(sign, 'big')
-            if decodedSign == 0:
-                sign = "+"
-            elif decodedSign == 1:
-                sign = "-"
-            elif decodedSign == 2:
-                sign = "*"
-            
-            calc = conn.recv(int.from_bytes(nb1Size, 'big')+int.from_bytes(nb2Size, 'big'))
-            nb1 = calc[:int.from_bytes(nb1Size, 'big')]
-            nb2 = calc[int.from_bytes(nb1Size, 'big'):int.from_bytes(nb1Size, 'big')+int.from_bytes(nb2Size, 'big')]
-            calcul = f"{int.from_bytes(nb1, 'big')}{sign}{int.from_bytes(nb2, 'big')}"
+            size = header[:2]            
+            calc = conn.recv(int.from_bytes(size, 'big'))
+            calcul = calc.decode()
             logger.info(f"Calcul reçu du client {addr} : {calcul}")
             answer = int(eval(calcul))
             if answer < 0:
