d601e686fe52943cf9bedcef26586e4eebaba2e8
Wiibleyde <nathan@bonnell.fr>
2023-12-08 09:22:18 +0100

    Add logging to chat server
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- TP6/chat_server_ii_6.py
+++ TP6/chat_server_ii_6.py
@@ -1,5 +1,7 @@
 import asyncio
 
+from src.logs import Logger
+
 global CLIENTS
 CLIENTS = {}
 
@@ -13,28 +15,32 @@ async def handle_client(reader, writer):
             CLIENTS[addr]["w"] = writer
             CLIENTS[addr]["r"] = reader
             CLIENTS[addr]["p"] = pseudo
-            print(f"Client {addr[0]}:{addr[1]} connected with pseudo {pseudo}")
+            logger.info(f"Client {addr[0]}:{addr[1]} connected with pseudo {pseudo}")
         elif data:
             message = data.decode()
             for client in CLIENTS:
+                logger.info(f"Message from {addr[0]}:{addr[1]} : {message}")
                 CLIENTS[client]["w"].write(f"{CLIENTS[addr]['p']} : {message}".encode())
                 await CLIENTS[client]["w"].drain()
         else:
             print(f"Client {addr!r} disconnected")
             for client in CLIENTS:
+                logger.info(f"Client {addr[0]}:{addr[1]} disconnected")
                 CLIENTS[client]["w"].write(f"{CLIENTS[addr]['p']} a quitté la chatroom".encode())
                 await CLIENTS[client]["w"].drain()
             del CLIENTS[addr]
             break
-    print("Close the connection")
+    logger.info("Close the connection")
     writer.close()
 
 async def main():
     server = await asyncio.start_server(handle_client, '127.0.0.1', 13337)
     addrs = ', '.join(str(s.getsockname()) for s in server.sockets)
-    print(f'Serving on {addrs}')
+    logger.info(f'Serving on {addrs}')
     async with server:
         await server.serve_forever()
 
 if __name__ == '__main__':
+    logger = Logger()
+    logger.info("Lancement du serveur")
     asyncio.run(main())
