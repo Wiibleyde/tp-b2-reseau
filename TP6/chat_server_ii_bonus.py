ec616981e6d43291c3822cea60fa993bb16e4bb1
Wiibleyde <nathan@bonnell.fr>
2023-12-08 10:01:30 +0100

    Add room functionality to handle multiple chat rooms
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- TP6/chat_server_ii_bonus.py
+++ TP6/chat_server_ii_bonus.py
@@ -1,11 +1,14 @@
 import asyncio
 import time
 import random
+import argparse
 
 from src.logs import Logger
+from src.config import Config
 
-global CLIENTS, TERMINAL_COLORS
+global CLIENTS, TERMINAL_COLORS, ROOMS
 
+ROOMS = {}
 CLIENTS = {}
 TERMINAL_COLORS = {
     "red": "\033[1;31m",
@@ -17,6 +20,8 @@ TERMINAL_COLORS = {
     "white": "\033[1;37m",
     "reset": "\033[0;0m",
 }
+HOST = ""
+PORT = 0
 
 async def handle_client(reader, writer) -> None:
     while True:
@@ -24,31 +29,39 @@ async def handle_client(reader, writer) -> None:
         addr = writer.get_extra_info('peername')
         if data.decode().startswith("Hello|"):
             pseudo = data.decode().split("|")[1]
+            room = data.decode().split("|")[2]
             CLIENTS[addr] = {}
             CLIENTS[addr]["w"] = writer
             CLIENTS[addr]["r"] = reader
             CLIENTS[addr]["p"] = pseudo
+            if room not in ROOMS.keys():
+                ROOMS[room] = []
+            ROOMS[room].append(addr)
             CLIENTS[addr]["c"] = chooseRandomColor()
             logger.info(f"Client {addr[0]}:{addr[1]} connected with pseudo {pseudo}")
         elif data:
             message = data.decode()
+            logger.info(f"Message from {addr[0]}:{addr[1]} : {message}")
             for client in CLIENTS:
-                logger.info(f"Message from {addr[0]}:{addr[1]} : {message}")
-                CLIENTS[client]["w"].write(f"{TERMINAL_COLORS[CLIENTS[addr]['c']]}[{get_time()}] {CLIENTS[addr]['p']} : {message}{TERMINAL_COLORS['reset']}".encode())
-                await CLIENTS[client]["w"].drain()
+                for room in ROOMS:
+                    if addr in ROOMS[room] and client in ROOMS[room]:        
+                        CLIENTS[client]["w"].write(f"{TERMINAL_COLORS[CLIENTS[addr]['c']]}[{get_time()}] {CLIENTS[addr]['p']} : {message}{TERMINAL_COLORS['reset']}".encode())
+                        await CLIENTS[client]["w"].drain()
         else:
             logger.info(f"Client {addr!r} disconnected")
             for client in CLIENTS:
                 logger.info(f"Client {addr[0]}:{addr[1]} disconnected")
-                CLIENTS[client]["w"].write(f"{CLIENTS[addr]['p']} a quitté la chatroom".encode())
-                await CLIENTS[client]["w"].drain()
+                for room in ROOMS:
+                    if addr in ROOMS[room] and client in ROOMS[room]:
+                        CLIENTS[client]["w"].write(f"{TERMINAL_COLORS['red']}[{get_time()}] {CLIENTS[addr]['p']} a quitté la chatroom{TERMINAL_COLORS['reset']}".encode())
+                        await CLIENTS[client]["w"].drain()
             del CLIENTS[addr]
             break
     logger.info("Close the connection")
     writer.close()
 
 async def main() -> None:
-    server = await asyncio.start_server(handle_client, '127.0.0.1', 13337)
+    server = await asyncio.start_server(handle_client, HOST, PORT)
     addrs = ', '.join(str(s.getsockname()) for s in server.sockets)
     logger.info(f'Serving on {addrs}')
     async with server:
@@ -63,9 +76,23 @@ def chooseRandomColor() -> str:
     while color == "reset" and cpt < len(TERMINAL_COLORS.keys()):
         color = random.choice(list(TERMINAL_COLORS.keys()))
         cpt += 1
+        if color not in [CLIENTS[client]["c"] for client in CLIENTS]:
+            break
     return color
 
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-ho", "--host", help="Host IP", type=str, default=config.get_host())
+    parser.add_argument("-p", "--port", help="Port", type=int, default=config.get_port())
+    return parser.parse_args()
+
 if __name__ == '__main__':
-    logger = Logger()
+    config = Config("./config.yml")
+    args = parse_args()
+    if args.host != config.get_host():
+        HOST = args.host
+    if args.port != config.get_port():
+        PORT = args.port
+    logger = Logger("/var/log/chat_room/server.log")
     logger.info("Lancement du serveur")
     asyncio.run(main())
