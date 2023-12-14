d15174ae79f6f1e5eeb183036b527db59cd95663
Wiibleyde <nathan@bonnell.fr>
2023-12-14 12:27:48 +0100

    Add support for Markdown formatting and emoji interpretation
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- TP7/ws_i_3_client.js
+++ TP7/ws_i_3_client.js
@@ -8,7 +8,7 @@ socket.addEventListener('message', function (event) {
     console.log('Message from WS Server: ', event.data);
     const response = document.getElementById('response');
     const p = document.createElement('p');
-    p.innerHTML = replaceEmojis(event.data);
+    p.innerHTML = interpreteMD(event.data);
     response.appendChild(p);
     response.scrollTop = response.scrollHeight;
 });
@@ -35,33 +35,81 @@ document.getElementById('message').addEventListener('keyup', function (event) {
     }
 });
 
-function replaceEmojis(message) {
-    const emojis = {
-        ':)': '🙂',
-        ':(': '🙁',
-        'xD': '😆',
-        ':o': '😮',
-        ':p': '😛',
-        ':D': '😀',
-        ':/': '😕',
-        ';)': '😉',
-        ':|': '😐',
-        '<3': '❤️',
-        ':*': '😘',
-        ':@': '😡',
-        ':s': '😖',
-        ':$': '😳',
-        ':L': '😫',
-        'B)': '😎',
-        ':X': '😶',
-        ':Z': '🤐',
-        ':O': '😲',
-        ':P': '😜',
-        ':S': '😖',
-        // Add more emojis here
-    };
-    for (let key in emojis) {
-        message = message.replace(key, emojis[key]);
+function interpreteMD(message) {
+    // Find all the emojis, the title and the bold, italic and underline text and replace them
+    // with the HTML tags
+    const emojis = message.match(/:([a-z]+):/g);
+    const title = message.match(/# (.+)/);
+    const title2 = message.match(/## (.+)/);
+    const title3 = message.match(/### (.+)/);
+    const bold = message.match(/\*\*(.+)\*\*/g);
+    const italic = message.match(/_(.+)_/g);
+    const underline = message.match(/__(.+)__/g);
+    if (emojis) {
+        for (let i = 0; i < emojis.length; i++) {
+            message = message.replace(emojis[i], getEmojiByName(emojis[i].replace(/:/g, '')));
+        }
+    }
+    if (title) {
+        message = message.replace(title[0], '<h1>' + title[1] + '</h1>');
+    }
+    if (title2) {
+        message = message.replace(title2[0], '<h2>' + title2[1] + '</h2>');
+    }
+    if (title3) {
+        message = message.replace(title3[0], '<h3>' + title3[1] + '</h3>');
+    }
+    if (bold) {
+        for (let i = 0; i < bold.length; i++) {
+            message = message.replace(bold[i], '<b>' + bold[i].replace(/\*\*/g, '') + '</b>');
+        }
+    }
+    if (italic) {
+        for (let i = 0; i < italic.length; i++) {
+            message = message.replace(italic[i], '<i>' + italic[i].replace(/_/g, '') + '</i>');
+        }
+    }
+    if (underline) {
+        for (let i = 0; i < underline.length; i++) {
+            message = message.replace(underline[i], '<u>' + underline[i].replace(/__/g, '') + '</u>');
+        }
     }
     return message;
+}
+
+function getEmojiByName(name) {
+    switch (name) {
+        case 'smile' || 'smiley':
+            return '😀';
+        case 'grin':
+            return '😁';
+        case 'joy':
+            return '😂';
+        case 'rofl':
+            return '🤣';
+        case 'sweat_smile':
+            return '😅';
+        case 'laughing':
+            return '😆';
+        case 'wink' || ';)':
+            return '😉';
+        case 'blush':
+            return '😊';
+        case 'yum':
+            return '😋';
+        case 'sunglasses':
+            return '😎';
+        case 'heart_eyes':
+            return '😍';
+        case 'kissing_heart':
+            return '😘';
+        case 'kissing':
+            return '😗';
+        case 'kissing_smiling_eyes':
+            return '😙';
+        case 'kissing_closed_eyes':
+            return '😚';
+        default:
+            return '';
+    }
 }
